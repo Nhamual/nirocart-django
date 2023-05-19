1b6ac09e7f2155c9dc9f66bf306b65e95db537c7
Clauter Nhamual <clauter.nino@outlook.com>
2023-05-19 19:17:01 +0800

    added cname to allowed host

--- nirocart/settings.py
+++ nirocart/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool) 
 
-ALLOWED_HOSTS = ['django-nirocart-env.eba-kbnmpn2d.us-west-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['django-nirocart1-env.eba-kmanm6et.us-west-2.elasticbeanstalk.com']
 
 
 # Application definition
